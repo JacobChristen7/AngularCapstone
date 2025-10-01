51166e64230e257400c75b3dfed81e9a8cf5b003
Jacob Christensen <jacobchristensen811@gmail.com>
2025-10-01 08:17:27 -0600

    added simple block dropping function

--- src/app/components/game/game.ts
+++ src/app/components/game/game.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
-  imports: [MatGridListModule, MatButton],
+  imports: [CommonModule, MatGridListModule, MatButton],
   templateUrl: './game.html',
   styleUrl: './game.css'
 })
 export class Game {
+  blocks = [
+    { id: 1 }
+  ];
+  nextId = 2;
 
+  dropBlock() {
+    this.blocks.unshift({ id: this.nextId++ });
+  }
 }
