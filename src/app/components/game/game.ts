e179352176ef6a52c3fd817341f7e8a810aa7426
Jacob Christensen <jacobchristensen811@gmail.com>
2025-10-01 08:37:44 -0600

    drop block function animates the proper block

--- src/app/components/game/game.ts
+++ src/app/components/game/game.ts
@@ -11,11 +11,15 @@ import { MatButton } from '@angular/material/button';
 })
 export class Game {
   blocks = [
-    { id: 1 }
+    {id: 1, animate: false }
   ];
   nextId = 2;
 
   dropBlock() {
-    this.blocks.unshift({ id: this.nextId++ });
+    this.blocks.unshift({ id: this.nextId++, animate: true});
+    const block = this.blocks.find(b => b.id === this.nextId - 2)
+    if (block) {
+      block.animate = false
+    }
   }
 }
