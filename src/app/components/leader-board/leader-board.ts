617840e067770c6fbcab0097cb195886aa577b6e
Jacob Christensen <jacobchristensen811@gmail.com>
2025-10-02 15:01:01 -0600

    added score placement ranges for leader board from top to bottom based on player scores array

--- src/app/components/leader-board/leader-board.ts
+++ src/app/components/leader-board/leader-board.ts
@@ -12,6 +12,18 @@ export class LeaderBoard {
   players = [
     { name: 'Swags', score: 230 },
     { name: 'Jake', score: 22 },
-    { name: 'Ben', score: 20 }
+    { name: 'Ben', score: 20 },
+    { name: 'James', score: 20 },
+    { name: 'Max', score: 18 },
+    { name: 'Sawyer', score: 17 },
+    { name: 'Swags Rival', score: 250 },
+    { name: 'Dave', score: 19 },
+    { name: 'Fail >.<', score: -100 },
+    { name: 'Juny', score: 19 },
+    { name: 'JC', score: 3 }
   ];
+
+  get sortedPlayers() {
+    return [...this.players].sort((a, b) => b.score - a.score);
+  }
 }
