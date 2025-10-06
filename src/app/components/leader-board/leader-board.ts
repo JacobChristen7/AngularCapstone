39499972bad1100b8d626cf5e621d83b8da661e1
David Granger <dpgranger8@gmail.com>
2025-10-06 09:58:07 -0600

    created ranking component

--- src/app/components/leader-board/leader-board.ts
+++ src/app/components/leader-board/leader-board.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScoreService, Score } from '../../services/score.service';
+import { Ranking } from '../ranking/ranking';
 import { Observable, map } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-leader-board',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, Ranking],
   templateUrl: './leader-board.html',
   styleUrl: './leader-board.css'
 })
 export class LeaderBoard {
+  
   scores$: Observable<Score[]>;
 
   constructor(private scoreService: ScoreService) {
---
+++ src/app/components/ranking/ranking.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking',
+  imports: [],
+  templateUrl: './ranking.html',
+  styleUrl: './ranking.css'
+})
+export class Ranking {
+  ranking = 0;
+  score = 0;
+
+  constructor() {
+    
+  }
+
+  getRanking() {
+    
+  }
+}
